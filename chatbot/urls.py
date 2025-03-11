1d4a2c7f63c59da9e9270c60f0233844f116e9fe
Islamic-analytics <analyticsislamic@gmail.com>
2025-03-12 03:57:56 +0800

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import chat_view, summary_view, fetch_chat_history, chat_detail, precheck_view
+from .views import chat_view, summary_view, fetch_chat_history, chat_detail, precheck_view, send_review
 
 urlpatterns = [
     # path('response/', chatbot_response, name='chatbot_response'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path("chat/", chat_view, name="chat_url"),
     path('chat-detail/<int:chat_id>/', chat_detail, name='chat_detail'),
     path('fetch-chat-history/', fetch_chat_history, name='fetch_chat_history'),
-    path('precheck/', precheck_view, name="precheck")
+    path('precheck/', precheck_view, name="precheck"),
+    path("send-review/", send_review, name="send_review"),
 ]
 
