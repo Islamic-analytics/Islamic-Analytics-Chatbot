4460644174bf315d12ce4b5b24b2b304eb19c4cf
Islamic-analytics <analyticsislamic@gmail.com>
2025-03-12 01:40:54 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -108,7 +108,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')  # Ensures Django picks up the env variable
+    )
 }
 
 
